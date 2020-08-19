7e7e2722b507c1396fe5714579eecb5419fc012d
sleepanda4523 <63851752+sleepanda4523@users.noreply.github.com>
2020-08-19 10:41:53 +0900

    Update login.py

--- login.py
+++ login.py
@@ -6,21 +6,18 @@ Created on Mon Aug  3 12:03:12 2020
 """
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
-from bs4 import BeautifulSoup
 import time
 
 url = 'https://www.dsm-dms.com'
-driver = webdriver.Chrome('C:\chromedriver.exe')
+driver = webdriver.Chrome
 driver.get(url)
 
 delay = 3
-
 al = Alert(driver)
 
 driver.implicitly_wait(delay)
 
 driver.find_element_by_xpath('//*[@id="meal"]/div/header/nav/button').click()
-
 driver.find_element_by_xpath('//*[@id="root"]/div[1]/div/div[2]/div[1]/input[1]').send_keys('sleepanda4523')
 driver.find_element_by_xpath('//*[@id="root"]/div[1]/div/div[2]/div[1]/input[2]').send_keys('juhong9227#')
 
@@ -30,7 +27,6 @@ time.sleep(1)
 al.accept()
 
 driver.find_element_by_xpath('//*[@id="apply"]/div/div[2]/a[1]/button').click()
-
 driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[1]/div[2]/div[6]').click()
 time.sleep(1)
 driver.find_element_by_xpath('/html/body/div/div/div[2]/div/div[2]/div/div[2]/div[1]/div/table/tr[4]/td[1]').click()
